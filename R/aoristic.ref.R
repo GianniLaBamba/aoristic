6ff910c83c5c1dc777401f49f6e77afae8d627fc
GianniLaBamba <33984358+GianniLaBamba@users.noreply.github.com>
2025-05-09 12:27:51 +0200

    Update aoristic.ref.R

--- R/aoristic.ref.R
+++ R/aoristic.ref.R
@@ -16,7 +16,7 @@ aoristic.ref <- function() {
     View <- NULL #prevents R CMD check flagging 'no visible global function definition'
     
     hour.reference <- data.frame(matrix(0, ncol = 7, nrow = 24))
-    colnames(hour.reference) <- c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
+    colnames(hour.reference) <- c("So", "Mo", "Di", "Mi", "Do", "Fr", "Sa")
     Range <- c("0000-0059", "0000-0159", "0200-0259", "0300-0359", "0400-0459", "0500-0559", "0600-0659", 
         "0700-0759", "0800-0859", "0900-0959", "1000-1059", "1100-1159", "1200-1259", "1300-1359", "1400-1459", 
         "1500-1559", "1600-1659", "1700-1759", "1800-1859", "1900-1959", "2000-2059", "2100-2159", "2200-2259", 
